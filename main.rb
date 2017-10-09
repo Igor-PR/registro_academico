4dbda5fa46c6e4d7fd23e33f568152620489f7cf
Larissa <larissanevesviana@gmail.com>
2017-10-09 16:15:10 -0300

    Funcao de saida do programa criada e loop para o menu principal

--- main.rb
+++ main.rb
@@ -5,36 +5,46 @@ load "matricula.rb"
 
 def menu
 
-	puts "Bem vindo ao sistema de gerenciamento acadêmico!"
-	puts "Digite o numero correspondente ao comando desejado"
-	puts "----------------------------------------------------"
-	puts "1 - Acessar menu Disciplinas "
-	puts "2 - Acessar menu Aluno "
-	puts "3 - Acessar menu Cursos "
-	puts "4 - Acessar menu Matricula "
-	puts "----------------------------------------------------"
+	menu = 0
+	puts "Bem vindo!"
 
-	menu = gets.to_i
-	#Le um inteiro
+	while(menu != 5) do
 
-	puts case menu
-	when 1
-		"Acessar menu Disciplinas"
-		disciplina()
-	when 2
-		"Acessar menu Aluno"
-	when 3
-		"Acessar menu Cursos"
-	when 4
-	 	"Acessar menu Matrícula"
-	else
-	 	"Você digitou #{menu} -- Esse comando não existe"
+		puts "\nSistema de gerenciamento acadêmico"
+		puts "Digite o numero correspondente ao comando desejado"
+		puts "----------------------------------------------------"
+		puts "1 - Acessar menu Disciplinas "
+		puts "2 - Acessar menu Aluno "
+		puts "3 - Acessar menu Cursos "
+		puts "4 - Acessar menu Matricula "
+		puts "5 - Encerrar o programa"
+		puts "----------------------------------------------------"
+
+		menu = gets.to_i
+		#Le um inteiro
+
+		case menu
+		when 1
+			puts "Acessar menu Disciplinas"
+			disciplina()
+		when 2
+			puts "\nAcessar menu Aluno"
+		when 3
+			puts "\nAcessar menu Cursos"
+		when 4
+		 	puts "\nAcessar menu Matrícula"
+		when 5
+		 	puts "\nTchau! :)"
+		 	exit
+		else
+		 	"Você digitou #{menu} -- Esse comando não existe"
+		end
 	end
 
 end
 
 def disciplina
-	puts "Disciplinas"
+	puts "\nDisciplinas"
 	puts "Digite o numero correspondente ao comando desejado"
 	puts "----------------------------------------------------"
 	puts "1 - Listar Disciplinas"
@@ -59,7 +69,10 @@ def disciplina
 		cargahoraria = gets.to_i
 		puts "Digite o valor da disciplina"
 		valor = gets.to_i
+
 		disciplina = Disciplina.new(codigo,nome,cargahoraria,valor)
+		$lista_disciplinas << disciplina
+
 	when 3
 		puts "Alterar Disciplina"
 	when 4
@@ -70,11 +83,11 @@ def disciplina
 
 end
 
+#variavel global
+$lista_disciplinas = Array.new
+
 #começo do código
 menu()
-
-
-
 
 
 #EXEMPLO DE COMO USAR UMA CLASSE
