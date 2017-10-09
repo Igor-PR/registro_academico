5ff5b90539d9880aa1c71aa1bf90088ebdaec880
Larissa <larissanevesviana@gmail.com>
2017-10-09 16:30:46 -0300

    Tela do terminal agora limpa sozinha e opção de Voltar implementada

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# registro_academico
-Trabalho da disciplina de Conceitos de Linguagens de Programação (2017/2)
+# Registro Acadêmico
 
-Para executar o código digite ruby main.rb
+Trabalho final da disciplina de Conceitos de Linguagens de Programação (2017/2)
+
+Para executar o código digite no terminal:
+ruby main.rb
--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ def menu
 	puts "Bem vindo!"
 
 	while(menu != 5) do
-
+		
 		puts "\nSistema de gerenciamento acadêmico"
 		puts "Digite o numero correspondente ao comando desejado"
 		puts "----------------------------------------------------"
@@ -22,6 +22,9 @@ def menu
 
 		menu = gets.to_i
 		#Le um inteiro
+
+		system "clear" 
+		#limpar a tela do terminal
 
 		case menu
 		when 1
@@ -37,54 +40,72 @@ def menu
 		 	puts "\nTchau! :)"
 		 	exit
 		else
-		 	"Você digitou #{menu} -- Esse comando não existe"
+		 	puts "\nVocê digitou #{menu} -- Esse comando não existe"
 		end
+		system "clear" 
 	end
 
 end
 
 def disciplina
-	puts "\nDisciplinas"
-	puts "Digite o numero correspondente ao comando desejado"
-	puts "----------------------------------------------------"
-	puts "1 - Listar Disciplinas"
-	puts "2 - Adicionar Disciplina"
-	puts "3 - Alterar Disciplina"
-	puts "4 - Remover Disciplina"
-	puts "----------------------------------------------------"
-	
-	menu = gets.to_i
-	#Le um inteiro
 
-	case menu
-	when 1
-		puts "Listar Disciplinas"
-	when 2
-		puts "Adicionar Disciplina"
-		puts "Digite o código da disciplina"
-		codigo = gets.to_i
-		puts "Digite o nome da disciplina"
-		nome = gets
-		puts "Digite a carga horária da disciplina"
-		cargahoraria = gets.to_i
-		puts "Digite o valor da disciplina"
-		valor = gets.to_i
+	menu = 0
+	while(menu != 5) do
 
-		disciplina = Disciplina.new(codigo,nome,cargahoraria,valor)
-		$lista_disciplinas << disciplina
+		puts "\nDisciplinas"
+		puts "Digite o numero correspondente ao comando desejado"
+		puts "----------------------------------------------------"
+		puts "1 - Listar Disciplinas"
+		puts "2 - Adicionar Disciplina"
+		puts "3 - Alterar Disciplina"
+		puts "4 - Remover Disciplina"
+		puts "5 - Voltar"
+		puts "----------------------------------------------------"
+		
+		menu = gets.to_i
+		#Le um inteiro
 
-	when 3
-		puts "Alterar Disciplina"
-	when 4
-		puts "Remover Disciplina"
-	else
-		puts "Você digitou #{menu} -- Esse comando não existe"
+		system "clear" 
+		#limpar a tela do terminal
+
+		case menu
+		when 1
+			puts "\nListar Disciplinas"
+			if ($lista_disciplinas.size == 0)
+				puts "Não há disciplinas cadastradas"
+			end
+		when 2
+			puts "\nAdicionar Disciplina"
+			puts "Digite o código da disciplina"
+			codigo = gets.to_i
+			puts "Digite o nome da disciplina"
+			nome = gets
+			puts "Digite a carga horária da disciplina"
+			cargahoraria = gets.to_i
+			puts "Digite o valor da disciplina"
+			valor = gets.to_i
+
+			disciplina = Disciplina.new(codigo,nome,cargahoraria,valor)
+			$lista_disciplinas << disciplina
+
+		when 3
+			puts "\nAlterar Disciplina"
+		when 4
+			puts "\nRemover Disciplina"
+		when 5
+			puts "\nVoltar"
+		else
+			puts "\nVocê digitou #{menu} -- Esse comando não existe"
+		end
 	end
 
 end
 
-#variavel global
+#vetores globais
 $lista_disciplinas = Array.new
+$lista_alunos = Array.new
+$lista_cursos = Array.new
+$lista_matricular = Array.new
 
 #começo do código
 menu()
